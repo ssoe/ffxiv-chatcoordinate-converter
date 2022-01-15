30379f247e007ff54eb71d4a618b5a4cf0c303df
SSoe <ssoe@users.noreply.github.com>
2022-01-15 17:42:17 +0100

    update output to write to file

--- CoordinateConverter.py
+++ CoordinateConverter.py
@@ -5,8 +5,10 @@ import re
 regex1 = "(Labyrinthos|Thavnair|Garlemald|Mare Lamentorum|Elpis|Ultima Thule)"
 regex2 = "\((\d|\s|\.|\,|\-)+\)"
 
+#opens output.txt, creating a file for writing data in
 #opens input.txt containing input data
-with open('input.txt') as text:
+with open("output.txt", "w") as output:
+  with open('input.txt') as text:
     for line in text:
       for match1 in re.finditer(regex1, line):
         zs = match1.start()
@@ -15,6 +17,7 @@ with open('input.txt') as text:
             cs = match2.start()
             ce = match2.end()
             #replace with output to file later
-            print("/coord " + (line[cs:ce]) + " : " + (line[zs:ze]))
+            output.write("/coord " + (line[cs:ce]) + " : " + (line[zs:ze]) + "\n")
  
 text.close()
+output.close
